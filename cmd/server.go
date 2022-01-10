5be1ec54591d660242ed74fb35d3f526f194e958
empenguin1186 <emperorpenguint1186@gmail.com>
2022-01-10 12:16:11 +0900

    :+1: get live API 実装

--- cmd/server.go
+++ cmd/server.go
@@ -11,33 +11,65 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
 const LAYOUT = "2006-01-02"
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		user := os.Getenv("USER")
-		pass := os.Getenv("PASS")
-		dataSourceName := fmt.Sprintf("%s:%s@tcp(localhost:3306)/sample?parseTime=true", user, pass)
-		db, err := sql.Open("mysql", dataSourceName)
-		defer db.Close()
-		checkErr(err, "db connection initialization failed.")
+	user := os.Getenv("USER")
+	pass := os.Getenv("PASS")
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(localhost:3306)/sample?parseTime=true", user, pass)
+	db, err := sql.Open("mysql", dataSourceName)
+	defer db.Close()
+	checkErr(err, "db connection initialization failed.")
 
-		liveRepository := infra.NewLiveRepositoryImpl(db)
-		bandRepository := infra.NewBandRepositoryImpl(db)
-		bandMemberRepository := infra.NewBandMemberRepositoryImpl(db)
+	liveRepository := infra.NewLiveRepositoryImpl(db)
+	bandRepository := infra.NewBandRepositoryImpl(db)
+	bandMemberRepository := infra.NewBandMemberRepositoryImpl(db)
+	liveDescService := domain.NewLiveDescServiceImpl(liveRepository, bandRepository, bandMemberRepository)
+	liveService := domain.NewLiveServiceImpl(liveRepository)
+
+	e := echo.New()
+	e.GET("/live", func(context echo.Context) error {
+		var start time.Time
+		err = echo.QueryParamsBinder(context).Time("start", &start, LAYOUT).BindError()
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+
+		var end time.Time
+		err = echo.QueryParamsBinder(context).Time("end", &end, LAYOUT).BindError()
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+
+		lives, err := liveService.GetByPeriod(&start, &end)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+		var liveResponse []*presentation.LiveResponse
+		for _, e := range lives {
+			liveResponse = append(liveResponse, presentation.NewLiveResponse(e))
+		}
+		return context.JSON(http.StatusOK, liveResponse)
+	})
+
+	e.GET("/live/:id", func(context echo.Context) error {
 		//playerRepository := infra.NewPlayerRepositoryImpl(db)
-		liveService := domain.NewLiveDescServiceImpl(liveRepository, bandRepository, bandMemberRepository)
 
 		//var date time.Time
-		//err = echo.QueryParamsBinder(c).Time("date", &date, LAYOUT).BindError()
-		var liveId int
-		err = echo.QueryParamsBinder(c).Int("live_id", &liveId).BindError()
-		checkErr(err, "Invalid Query Parameter")
-		liveModel, err := liveService.GetById(liveId)
-		return c.JSON(http.StatusOK, presentation.NewLiveResponse(liveModel))
+		//err = echo.QueryParamsBinder(context).Time("date", &date, LAYOUT).BindError()
+		liveId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+		liveModel, err := liveDescService.GetById(int(liveId))
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+		return context.JSON(http.StatusOK, presentation.NewLiveDescResponse(liveModel))
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- presentation/live_response.go
+++ presentation/live_response.go
@@ -6,6 +6,32 @@ import (
 )
 
 type LiveResponse struct {
+	// ライブID
+	Id int `json:"id"`
+	// ライブ名
+	Name string `json:"name"`
+	// 場所
+	Location string `json:"location"`
+	// 日付
+	Date time.Time `json:"date"`
+	// 1人あたりの出演料
+	PerformanceFee int `json:"performance_fee"`
+	// 1バンドあたりの機材費
+	EquipmentCost int `json:"equipment_cost"`
+}
+
+func NewLiveResponse(live *domain.Live) *LiveResponse {
+	return &LiveResponse{
+		Id:             live.Id,
+		Name:           live.Name,
+		Location:       live.Location,
+		Date:           live.Date,
+		PerformanceFee: live.PerformanceFee,
+		EquipmentCost:  live.EquipmentCost,
+	}
+}
+
+type LiveDescResponse struct {
 	// ライブ名
 	Name string `json:"name"`
 	// 場所
@@ -20,7 +46,7 @@ type LiveResponse struct {
 	Band []*BandResponsePart `json:"band,omitempty"`
 }
 
-func NewLiveResponse(liveModel *domain.LiveModel) *LiveResponse {
+func NewLiveDescResponse(liveModel *domain.LiveModel) *LiveDescResponse {
 	bands := liveModel.Band
 	var bandResponseParts []*BandResponsePart
 	for _, band := range bands {
@@ -37,7 +63,7 @@ func NewLiveResponse(liveModel *domain.LiveModel) *LiveResponse {
 			Member: memberResponseParts,
 		})
 	}
-	return &LiveResponse{
+	return &LiveDescResponse{
 		Name:           liveModel.Name,
 		Location:       liveModel.Location,
 		Date:           liveModel.Date,
