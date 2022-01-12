cecb5cf42cb8b33662c75dad911c40ad61131c7c
empenguin1186 <emperorpenguint1186@gmail.com>
2022-01-12 22:52:31 +0900

    :recycle: リファクタ

--- cmd/server.go
+++ cmd/server.go
@@ -12,15 +12,15 @@ import (
 	"os"
 )
 
-const LAYOUT = "2006-01-02"
-
 func main() {
 	user := os.Getenv("USER")
 	pass := os.Getenv("PASS")
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(localhost:3306)/sample?parseTime=true", user, pass)
 	db, err := sql.Open("mysql", dataSourceName)
 	defer db.Close()
-	checkErr(err, "db connection initialization failed.")
+	if err != nil {
+		log.Fatalln("db connection initialization failed.", err)
+	}
 
 	liveRepository := infra.NewLiveRepositoryImpl(db)
 	bandRepository := infra.NewBandRepositoryImpl(db)
@@ -31,10 +31,4 @@ func main() {
 	e := echo.New()
 	server := presentation.NewLiveServer(e, liveService, liveDescService)
 	server.Start()
-}
-
-func checkErr(err error, msg string) {
-	if err != nil {
-		log.Fatalln(msg, err)
-	}
 }
